76c63a56c378801bc4c35ce5a7c0f8cf4d7df2bd
dharanigowthamsampath <dharanigowthamsampath@gmail.com>
2024-11-12 10:43:56 +0530

    updated Main file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.add(10); // ctrl+d for duplicate
+        list.add(10);
         list.add(20);
-        list.add(30);
         list.add(40);
         list.add(50);
+        list.add(60);
     }
 }
